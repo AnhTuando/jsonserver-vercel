195f9a5338eb80cf89ed65453fb0883ee3b780bb
AnhTuando <96539363+AnhTuando@users.noreply.github.com>
2023-06-09 20:20:54 +0700

    upload30

--- public/js/others.js
+++ public/js/others.js
@@ -1,6 +1,6 @@
 // Call Foods API
 async function getFoods() {
-  let res = await fetch("http://localhost:3000/foods");
+  let res = await fetch("foods");
   let data = await res.json();
   return data;
 }
